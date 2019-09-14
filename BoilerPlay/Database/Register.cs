3743dba7c605c3e28ebfca0ed34b9a8589b7d7fb
A-Wadhwani <wadhwani@purdue.edu>
2019-09-14 15:45:09 -0400

    Created Register Class and Function to if Email is Used

--- BoilerPlay/Database/Register.cs
+++ BoilerPlay/Database/Register.cs
@@ -1,11 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 
 namespace BoilerPlay.Database
 {
-    public class Register
+    public static class Register
     {
+        public static bool IsEmailUsed(String email)
+        {
+            DataTable output = Database.Query.ExecuteReturnCommand("select Email, Password from Accounts WHERE Email = '" + email + "';");
+            try
+            {
+                String pass = output.Rows[0].ItemArray[1].ToString();
+                return false;
+            }
+            catch(Exception e)
+            {
+                return true;
+            }
+        }
+        public static void RegisterAccount(String Name,int Desc,String Password,String Email, String PhoneNo)
+        {
+
+        }
     }
 }
