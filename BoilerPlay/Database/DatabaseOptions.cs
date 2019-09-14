747509ad45d8b24a1e3a63016a97a4a2badcb0c2
mmahaja <mmahaja@purdue.edu>
2019-09-14 15:59:48 -0400

    Updated

--- BoilerPlay/Database/DatabaseOptions.cs
+++ BoilerPlay/Database/DatabaseOptions.cs
@@ -19,5 +19,16 @@ namespace BoilerPlay.Database
             public static readonly string Skilled = "Skilled";
             public static readonly string Professional = "Professional";
         }
+        public static class Posts_Sports
+        {
+            public static readonly string Table_Tennis = "Table Tennis";
+            public static readonly string Tennis = "Tennis";
+            public static readonly string Football = "Football";
+            public static readonly string Cricket = "Cricket";
+            public static readonly string Basketball = "Basketball";
+            public static readonly string Pool = "Pool";
+            public static readonly string Air_Hockey = "Air_Hockey";
+            public static readonly string Cardgames = "Cardgames";
+        }
     }
 }
