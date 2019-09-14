2f2d5bc0b886a5d5134341bf9361d3d72e1cc0b7
A-Wadhwani <wadhwani@purdue.edu>
2019-09-14 19:49:55 -0400

    Finished Register Function

--- BoilerPlay/Database/Register.cs
+++ BoilerPlay/Database/Register.cs
@@ -26,8 +26,33 @@ namespace BoilerPlay.Database
             if (!IsEmailUsed(Email))
             {
                 int ID = Database.HelloWorldQueryMethods.GenerateNewProfileID();
-                
-                Database.Query.ExecuteNonReturnCommand("insert into Accounts VALUES ");
+                int yr;
+                switch (Year)
+                {
+                    case "Freshman":
+                        yr = 0;
+                        break;
+                    case "Sophomore":
+                        yr = 1;
+                        break;
+                    case "Junior":
+                        yr = 2;
+                        break;
+                    case "Senior":
+                    default:
+                        yr = 3;
+                        break;
+                }
+                //Database.Query.ExecuteNonReturnCommand("insert into Accounts VALUES('" + ID + "','" + Name + "');
+                Query.ExecuteNonReturnCommand(String.Format("INSERT INTO Accounts" +
+                   "VALUES ('{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}');",
+                    ID,
+                    Name,
+                    yr,
+                    Desc,
+                    Password,
+                    Email,
+                    PhoneNo));
             }
         }
     }
